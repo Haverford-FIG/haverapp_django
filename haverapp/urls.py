fbd7c0e5a9f5936b26c9f2e7460f708ee8800d72
Casey Falk <casey@ironwood.haverford.edu>
2014-03-20 02:06:29 -0400

    Fixed bug in student_news.

--- haverapp/urls.py
+++ haverapp/urls.py
@@ -21,23 +21,19 @@ urlpatterns = patterns('',
 	(r"^events_swarthmore?/?$", events, {"page": "swarthmore"}),
 	(r"^events_upenn/?$", events, {"page": "upenn"}),
 	(r"^events_campus_philly/?$", events, {"page": "campus_philly"}),
-	(r"^transportation/?$", menu_screen, {"page": "transportation"}),
-	
 	
 	(r"^student_?news?/?$", menu_screen, {"page": "studentnews"}),
 	(r"^haverford_?news?/?$", news, {"page": "haverfordnews"}),
 
+	(r"^transportation/?$", menu_screen, {"page": "transportation"}),
 	(r"^SEPTA/?$", transportation, {"page": "SEPTA", "option":"Haverford"}),
 	(r"^SEPTA_[Hh]averford/?$", transportation, {"page": "SEPTA", "option":"Haverford"}),
 	(r"^SEPTA_[aA]rdmore/?$", transportation, {"page": "SEPTA", "option":"Ardmore"}),
 	(r"^[bB]lue[bB]us/?$", transportation, {"page": "bluebus"}),
 	(r"^[bB]lue[bB]us/(?P<month>\d{2})/(?P<day>\d{2})/(?P<year>\d{4})/?$", transportation, {"page": "bluebus"}),
+
 #	(r"^studentnews/?$", studentnews, {"page":"studentnews"}),
 	(r"^dining/?$", get_DC_menu),	
-	#Test Views that ultimately should be deleted.
-	(r"^camp_philly_feed/?$", camp_philly_feed),
-
-	(r"^my_main_page/?$", main_page),
 	
     # Uncomment the next line to enable the admin:
     	(r'^admin/', include(admin.site.urls)),
